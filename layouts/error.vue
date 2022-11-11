8b9c46741bed1d2fd4fa394ee3a418a1cd127b5f
Cimorexave <sadeq.s502@gmail.com>
2022-11-11 16:46:50 +0330

    feat: updated fallback for error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="p-20 dark:bg-slate-800 dark:text-white h-screen bg-gray-100">
-    Error: {{ error.statusCode }} <br>
-    {{ error.message }}
+    Error: {{ error.statusCode }} <br />
+    {{ error.message }} <br />
+    <nuxt-link to="/">Back</nuxt-link>
   </div>
 </template>
 <script>
