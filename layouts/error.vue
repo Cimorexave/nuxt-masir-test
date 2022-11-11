6dae6e3df3279b91c78ebf3e3d07a5de49959c0f
Cimorexave <sadeq.s502@gmail.com>
2022-11-11 15:49:26 +0330

    feat: updated error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>Error Page</div>
+  <div class="p-20">
+    Error: {{ error.statusCode }} <br>
+    {{ error.message }}
+  </div>
 </template>
 <script>
 export default {
   props: ["error"],
   layout: "layout",
+  mounted() {
+    console.log(this.error);
+  },
 };
 </script>
